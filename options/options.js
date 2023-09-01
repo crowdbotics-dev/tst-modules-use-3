cfc45bfdd168185c992333845f1fe8889158c57a
Saurav Gharti Magar <saurav@crowdbotics.com>
2023-09-01 18:56:37 +0545

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "tst-modules-use-3",
-  url: "https://tst-modules-use-3.botics.co",
-  api: "https://tst-modules-use-3.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "tst-modules-use-3",
+  "url": "https://tst-modules-use-3.botics.co",
+  "api": "https://tst-modules-use-3.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "aboutTheApp"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
